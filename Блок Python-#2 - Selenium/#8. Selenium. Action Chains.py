e7351002230d8f1ac3ad43c56c475557a3becaf2
User <Vancha-15@mail.ru>
2026-03-26 21:24:18 +0300

    Практика.Selenium.

--- Блок Python-#2 - Selenium/#8. Selenium. Action Chains.py
+++ Блок Python-#2 - Selenium/#8. Selenium. Action Chains.py
@@ -27,3 +27,32 @@ time.sleep(2)
 
 element = driver.find_element(...)
 action.click(element).preform() #Одинарный клик левой кнопкой мыши
+
+
+
+#Для наведения на элемент, action использует метод move_to_element(),
+#Где в качестве аргумента принимает веб-элемент для наведения.
+#
+# STEP_1_LOCATOR = ("xpath", "//a[text()='Main Item 2']")
+# STEP_2_LOCATOR = ("xpath", "//a[text()='SUB SUB LIST »']")
+# STEP_3_LOCATOR = ("xpath", "//a[text()='Sub Sub Item 2']")
+# STEP_1 = driver.find_element(*STEP_1_LOCATOR)
+# STEP_2 = driver.find_element(*STEP_2_LOCATOR)
+# STEP_3 = driver.find_element(*STEP_3_LOCATOR)
+#action.move_to_element(STEP_1).move_to_element(STEP_2).move_to_element(STEP_3).perform()
+#
+# Если нужна пауза между цепочками, то можем написать так:
+# action.move_to_element(STEP_1).pause(3).move_to_element(STEP_2).pause(3).move_to_element(STEP_3).pause(3).perform()
+
+# Перетаскивание элементов (drag and drop)
+# Нам нужно указать элемент, который мы будем перетаскивать и область в которую будем его перетаскивать.
+# Как правило:
+# source - это то, что мы перетягиваем.
+# target - это то, куда мы перетягиваем.
+#
+#driver.get("https://demoqa.com/droppable")
+# SOURCE_LOCATOR = ("xpath", "//div[@id='draggable']")
+# TARGET_LOCATOR = ("xpath", "//div[@id='droppable']")
+# SOURCE = driver.find_element(*SOURCE_LOCATOR)
+# TARGET = driver.find_element(*TARGET_LOCATOR)
+# action.drag_and_drop(SOURCE, TARGET).perform()  # Перетаскивание
