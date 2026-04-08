d026a0f308cefc4dda4152e6a9069a2c29289ebb
User <Vancha-15@mail.ru>
2026-04-08 21:30:30 +0300

    Проект.Selenium, PyTest.

--- Практика/test_my2.py
+++ Практика/test_my2.py
@@ -1,15 +1,28 @@
+# pytest практика / test_my2.py -sv --lf
+
+import pytest
 from selenium import webdriver
 
-class TestLogin: # Название тестового класса
+class TestLogin:    # Название тестового класса
 
     def setup_method(self):
-        print("Выполняюсь до теста")
         self.driver = webdriver.Chrome()
 
-    def test_open_login_page(self):
+    @pytest.mark.smoke
+    def test_open_login_page(self):     # Тест пройдет
         self.driver.get("https://demoqa.com/login")
         assert self.driver.current_url == "https://demoqa.com/login", "Ошибка"
 
+    @pytest.mark.regression
+    def test_open_books_page(self):     # Тест упадет
+        self.driver.get("https://demoqa.com/books")
+        assert self.driver.current_url == "https://", "Ошибка"
+
+    @pytest.mark.smoke  # Тест относится и к smoke и к sanity
+    @pytest.mark.sanity
+    def test_open_profile_page(self):     # Тест упадет
+        self.driver.get("https://demoqa.com/profile")
+        assert self.driver.current_url == "https://", "Ошибка"
+
     def teardown_method(self):
         self.driver.close()
-        print("Выполняюсь после теста")
