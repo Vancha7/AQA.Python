08e4757ef5298568a8b5959832b3c6c6a954773d
User <Vancha-15@mail.ru>
2026-04-22 21:04:32 +0300

    Проект Dem_QA.

--- Практика/test_example.py
+++ Практика/test_example.py
@@ -1,11 +1,35 @@
 import pytest
+import allure
+from allure_commons.types import Severity
 
 
-# Вызываем фикстуру над классом
-class TestExample:
+@allure.epic("Accounts")
+@allure.feature("Login")
+@allure.story("Pages")
+@pytest.mark.usefixtures("driver")
+class TestPages:
 
-    @pytest.mark.usefixtures("generate_data")
-    def test_example_cls(self):
-        # Обращаясь через self к данным, мы легко получаем к ним доступ
-        print(self.login, self.password)
-###
+
+    @pytest.mark.smoke
+    @allure.title("Open login page")
+    @allure.severity(Severity.NORMAL)
+    @allure.link(url="https://confluence.com", name="Documentation")
+    def test_open_login_page(self):
+        self.driver.get("https://demoqa.com/login")
+        assert self.driver.current_url == "https://demoqa.com/login", "Ошибка ULR страницы входа"
+
+    @pytest.mark.regression
+    @allure.title("Open book page")
+    @allure.severity(Severity.NORMAL)
+    @allure.link(url="https://confluence.com", name="Documentation")
+    def test_open_books_page(self):
+        self.driver.get("https://demoqa.com/books")
+        assert self.driver.current_url == "https://demoqa.com/books", "Ошибка ULR страницы с книгами"
+
+    @pytest.mark.profile
+    @allure.title("Open profile page")
+    @allure.severity(Severity.NORMAL)
+    @allure.link(url="https://confluence.com", name="Documentation")
+    def test_open_profile_page(self):
+        self.driver.get("https://demoqa.com/profile")
+        assert self.driver.current_url == "https://demoqa.com/profile", "Ошибка ULR страницы профиля"
